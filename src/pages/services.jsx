1dd1e3cc65436bac197db5eb56f52ce3ce0302cf
adraawen <bauvais.n@protonmail.com>
2025-12-20 22:34:01 +0100

    mentions pages with Bootstrap accordion + adding banner to different pages

--- src/pages/services.jsx
+++ src/pages/services.jsx
@@ -1,55 +1,62 @@
 import "../index.css"
+import Banner from "../composents/banner";
 
 function Services(){
     return(
-    <div className="services-page">
-        <h4 className="portfolio-title">Mes services</h4>
-        <p className="portfolio-text">Mes services et prestations.</p>
-        
+        <>
+            <Banner
+                image="/assets/images/banner.jpg"
+        />
 
-        <div className="container-fluid">
-            <div className="row justify-content-center g-4 mx-5">
-                <div className="col col-12 col-md-6 col-lg-4 mb-4">
-                    <div className="card" style={{width:"100%"}}>
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi-services bi-file-earmark-code" viewBox="0 0 16 16">
-                            <path d="M14 4.5V14a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2h5.5zm-3 0A1.5 1.5 0 0 1 9.5 3V1H4a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1V4.5z"/>
-                            <path d="M8.646 6.646a.5.5 0 0 1 .708 0l2 2a.5.5 0 0 1 0 .708l-2 2a.5.5 0 0 1-.708-.708L10.293 9 8.646 7.354a.5.5 0 0 1 0-.708m-1.292 0a.5.5 0 0 0-.708 0l-2 2a.5.5 0 0 0 0 .708l2 2a.5.5 0 0 0 .708-.708L5.707 9l1.647-1.646a.5.5 0 0 0 0-.708"/>
-                        </svg>
-                        <div className="card-body">
-                                <h5 className="card-title">Developpement Web</h5>
-                                <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+        <div className="services-page">
+            <h4 className="portfolio-title">Mes services</h4>
+            <p className="portfolio-text">Mes services et prestations.</p>
+            
+
+            <div className="container-fluid">
+                <div className="row justify-content-center g-4 mx-5">
+                    <div className="col col-12 col-md-6 col-lg-4 mb-4">
+                        <div className="card" style={{width:"100%"}}>
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi-services bi-file-earmark-code" viewBox="0 0 16 16">
+                                <path d="M14 4.5V14a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2h5.5zm-3 0A1.5 1.5 0 0 1 9.5 3V1H4a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1V4.5z"/>
+                                <path d="M8.646 6.646a.5.5 0 0 1 .708 0l2 2a.5.5 0 0 1 0 .708l-2 2a.5.5 0 0 1-.708-.708L10.293 9 8.646 7.354a.5.5 0 0 1 0-.708m-1.292 0a.5.5 0 0 0-.708 0l-2 2a.5.5 0 0 0 0 .708l2 2a.5.5 0 0 0 .708-.708L5.707 9l1.647-1.646a.5.5 0 0 0 0-.708"/>
+                            </svg>
+                            <div className="card-body">
+                                    <h5 className="card-title">Developpement Web</h5>
+                                    <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                            </div>
                         </div>
                     </div>
-                </div>
-        
-                <div className="col col-12 col-md-6 col-lg-4 mb-4">
-                    <div className="card" style={{width:"100%"}}>
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" className="bi-services bi-palette" viewBox="0 0 16 16">
-                            <path d="M8 5a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3m4 3a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3M5.5 7a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0m.5 6a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3"/>
-                            <path d="M16 8c0 3.15-1.866 2.585-3.567 2.07C11.42 9.763 10.465 9.473 10 10c-.603.683-.475 1.819-.351 2.92C9.826 14.495 9.996 16 8 16a8 8 0 1 1 8-8m-8 7c.611 0 .654-.171.655-.176.078-.146.124-.464.07-1.119-.014-.168-.037-.37-.061-.591-.052-.464-.112-1.005-.118-1.462-.01-.707.083-1.61.704-2.314.369-.417.845-.578 1.272-.618.404-.038.812.026 1.16.104.343.077.702.186 1.025.284l.028.008c.346.105.658.199.953.266.653.148.904.083.991.024C14.717 9.38 15 9.161 15 8a7 7 0 1 0-7 7"/>
-                        </svg>
-                        <div className="card-body">
-                                <h5 className="card-title">UX Design</h5>
-                                <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+            
+                    <div className="col col-12 col-md-6 col-lg-4 mb-4">
+                        <div className="card" style={{width:"100%"}}>
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" className="bi-services bi-palette" viewBox="0 0 16 16">
+                                <path d="M8 5a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3m4 3a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3M5.5 7a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0m.5 6a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3"/>
+                                <path d="M16 8c0 3.15-1.866 2.585-3.567 2.07C11.42 9.763 10.465 9.473 10 10c-.603.683-.475 1.819-.351 2.92C9.826 14.495 9.996 16 8 16a8 8 0 1 1 8-8m-8 7c.611 0 .654-.171.655-.176.078-.146.124-.464.07-1.119-.014-.168-.037-.37-.061-.591-.052-.464-.112-1.005-.118-1.462-.01-.707.083-1.61.704-2.314.369-.417.845-.578 1.272-.618.404-.038.812.026 1.16.104.343.077.702.186 1.025.284l.028.008c.346.105.658.199.953.266.653.148.904.083.991.024C14.717 9.38 15 9.161 15 8a7 7 0 1 0-7 7"/>
+                            </svg>
+                            <div className="card-body">
+                                    <h5 className="card-title">UX Design</h5>
+                                    <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="col col-12 col-md-6 col-lg-4 mb-4">
+                        <div className="card" style={{width:"100%"}}>
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" className="bi-services bi-search" viewBox="0 0 16 16">
+                                <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0"/>
+                            </svg>
+                            <div className="card-body">
+                                    <h5 className="card-title">SEO</h5>
+                                    <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                            </div>
                         </div>
                     </div>
                 </div>
 
-                <div className="col col-12 col-md-6 col-lg-4 mb-4">
-                    <div className="card" style={{width:"100%"}}>
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" className="bi-services bi-search" viewBox="0 0 16 16">
-                            <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0"/>
-                        </svg>
-                        <div className="card-body">
-                                <h5 className="card-title">SEO</h5>
-                                <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-                        </div>
-                    </div>
-                </div>
             </div>
-
         </div>
-    </div>
+        </>
     )
 }
 
