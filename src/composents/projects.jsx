5e7d0dc9a9aab0ca41f5fa4fd8d500095c7efd4f
adraawen <bauvais.n@protonmail.com>
2025-12-20 18:36:45 +0100

    add links in footer portolfio section

--- src/composents/projects.jsx
+++ src/composents/projects.jsx
@@ -5,16 +5,22 @@ function Projects({ variant }) {
   return (
     <ul className={`navbar-nav ${variant === "footer" ? "footer-menu" : "me-auto mb-2 mb-lg-0"}`}>
       <li className="nav-item">
-        <NavLink to="/realisations" className="nav-link">Réalisations</NavLink>
+        <a href="https://www.espacebienetre-ista.com/" target="_blank" rel="noopener noreferrer" className="nav-link">Espace bien-être</a>
       </li>
       <li className="nav-item">
-        <NavLink to="/services" className="nav-link">Services</NavLink>
+        <a href="https://www.freshfood.fr/" target="_blank" rel="noopener noreferrer" className="nav-link">Fresh Food</a>
       </li>
       <li className="nav-item">
-        <NavLink to="/contact" className="nav-link">Contact</NavLink>
+        <a href="https://www.lessakura.fr/" target="_blank" rel="noopener noreferrer" className="nav-link">Sakura Ya</a>
       </li>
       <li className="nav-item">
-        <NavLink to="/mentions" className="nav-link">Mentions</NavLink>
+        <a href="https://www.respire.co/" target="_blank" rel="noopener noreferrer" className="nav-link">SEO respire</a>
+      </li>
+      <li className="nav-item">
+        <a href="https://www.figma.com/proto/yZMwdCazL45pAoveY3g6BU/Probeats---Maquette?node-id=17-135&t=3BqjTjkYBCGGABYj-1&starting-point-node-id=17%3A135" target="_blank" rel="noopener noreferrer" className="nav-link">Maquette Probeats</a>
+      </li>
+      <li className="nav-item">
+        <a href="https://portail-api.meteofrance.fr/web/fr/api/DonneesPubliquesObservation" target="_blank" rel="noopener noreferrer" className="nav-link">API météo</a>
       </li>
     </ul>
   );
